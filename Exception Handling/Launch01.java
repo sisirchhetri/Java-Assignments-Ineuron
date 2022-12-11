befe5b6e43c8930a6d464f232ba5652c541f0206
Sisir Chhetri <75107202+sisirchhetri@users.noreply.github.com>
2022-12-11 19:25:58 +0530

    added launch01.java

---
+++ Exception Handling/Launch01.java
@@ -0,0 +1,27 @@
+package in.sisirchhetri;
+
+import java.util.Scanner;
+
+public class Launch01 {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+		System.out.println("Welcome to division performing application");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the first Number to divide");
+        int a=scan.nextInt();
+        System.out.println("Enter the second number");
+        int b=scan.nextInt();
+        int res=a/b;
+        System.out.println("The result is "+ res);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Please provide non zero denominotor");
+		}
+		 System.out.println("Thank you for using our application");  
+	}
+
+}
